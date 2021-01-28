c65f734bb7ec9f39dc014e8d5b86d2abac0f8363
Nicholas Corgan <n.corgan@gmail.com>
2021-01-28 17:18:07 -0600

    AFF3CT coder base class

---
+++ Source/AFF3CT/CoderBase.cpp
@@ -0,0 +1,76 @@
+// Copyright (c) 2021 Nicholas Corgan
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "CoderBase.hpp"
+
+#include <Pothos/Framework.hpp>
+
+template <typename B, typename Q>
+AFF3CTCoderBase<B,Q>::AFF3CTCoderBase(CoderType coderType):
+    _coderType(coderType),
+    _codecUPtr(nullptr)
+{
+    static const Pothos::DType dtype(typeid(B));
+
+    this->setupInput(0, dtype);
+    this->setupOutput(0, dtype);
+}
+
+template <typename B, typename Q>
+AFF3CTCoderBase<B,Q>::~AFF3CTCoderBase()
+{
+}
+
+template <typename B, typename Q>
+void AFF3CTCoderBase<B,Q>::work()
+{
+    const auto elems = this->workInfo().minElements;
+    if(0 == elems) return;
+
+    switch(_coderType)
+    {
+        case CoderType::ENCODER:
+            this->_workEncoder();
+            break;
+
+        case CoderType::DECODER_SISO:
+            this->_workDecoderSISO();
+            break;
+
+        case CoderType::DECODER_SIHO:
+            this->_workDecoderSIHO();
+            break;
+
+        case CoderType::DECODER_HIHO:
+            this->_workDecoderHIHO();
+            break;
+
+        default:
+            throw Pothos::AssertionViolationException("Invalid internal coder type enum: "+std::to_string(int(_coderType)));
+    }
+}
+
+template <typename B, typename Q>
+void AFF3CTCoderBase<B,Q>::_workEncoder()
+{
+}
+
+template <typename B, typename Q>
+void AFF3CTCoderBase<B,Q>::_workDecoderSISO()
+{
+}
+
+template <typename B, typename Q>
+void AFF3CTCoderBase<B,Q>::_workDecoderSIHO()
+{
+}
+
+template <typename B, typename Q>
+void AFF3CTCoderBase<B,Q>::_workDecoderHIHO()
+{
+}
+
+template class AFF3CTCoderBase<B_8,Q_8>;
+template class AFF3CTCoderBase<B_16,Q_16>;
+template class AFF3CTCoderBase<B_32,Q_32>;
+template class AFF3CTCoderBase<B_64,Q_64>;
---
+++ Source/AFF3CT/CoderBase.hpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2021 Nicholas Corgan
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <Pothos/Framework.hpp>
+
+#include <aff3ct.hpp>
+
+#include <memory>
+
+template <typename B, typename Q>
+class AFF3CTCoderBase: public Pothos::Block
+{
+public:
+    using Class = AFF3CTCoderBase<B,Q>;
+    using CodecUPtr = std::unique_ptr<aff3ct::module::Codec<B,Q>>;
+
+    enum class CoderType
+    {
+        ENCODER,
+        DECODER_SISO,
+        DECODER_SIHO,
+        DECODER_HIHO
+    };
+
+    AFF3CTCoderBase(CoderType coderType);
+    virtual ~AFF3CTCoderBase();
+
+    virtual void work();
+
+protected:
+    CoderType _coderType;
+    CodecUPtr _codecUPtr;
+
+    void _workEncoder();
+    void _workDecoderSISO();
+    void _workDecoderSIHO();
+    void _workDecoderHIHO();
+};
